8c6afa8b75e00e3726d6357c14f2511cb9335711
kovacs <mritd@linux.com>
2024-01-09 10:49:07 +0800

    feat(pkgs): add firewall
    add firewall
    
    Signed-off-by: kovacs <mritd@linux.com>

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 PROFILE="generic"
 
 # qemu-ga?
-PACKAGES="-firewall -odhcp6c -odhcpd-ipv6only -ppp -ppp-mod-pppoe ca-certificates kmod-tun open-vm-tools"
+PACKAGES="-odhcp6c -odhcpd-ipv6only -ppp -ppp-mod-pppoe ca-certificates kmod-tun open-vm-tools"
 
 FILES="files"
 
-DISABLED_SERVICES=""
+DISABLED_SERVICES="firewall"
 
 make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES" DISABLED_SERVICES="$DISABLED_SERVICES"
